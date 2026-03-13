a3a9d7ebdc4f99c96069a21039da4701928c5c4e
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2026-03-14 00:04:08 +0530

    refactor(test): switch TempTestDir to tempfile crate

--- src/test_util.rs
+++ src/test_util.rs
@@ -1,29 +1,23 @@
 #[cfg(test)]
 pub(crate) struct TempTestDir {
-    pub path: std::path::PathBuf,
+    dir: tempfile::TempDir,
 }
 
 #[cfg(test)]
 impl TempTestDir {
     pub fn new(name: &str) -> Self {
-        let path = std::env::temp_dir().join(name);
-        let _ = std::fs::remove_dir_all(&path);
-        std::fs::create_dir_all(&path).unwrap();
-        Self { path }
+        let dir = tempfile::Builder::new()
+            .prefix(name)
+            .tempdir()
+            .unwrap();
+        Self { dir }
     }
 
     pub fn create_file(&self, name: &str, content: &str) {
-        std::fs::write(self.path.join(name), content).unwrap();
+        std::fs::write(self.dir.path().join(name), content).unwrap();
     }
 
     pub fn path(&self) -> &std::path::Path {
-        &self.path
-    }
-}
-
-#[cfg(test)]
-impl Drop for TempTestDir {
-    fn drop(&mut self) {
-        let _ = std::fs::remove_dir_all(&self.path);
+        self.dir.path()
     }
 }
